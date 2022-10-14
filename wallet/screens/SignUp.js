7ae626145e0ce3e7ee159dd0362f6818d0a987f0
Harshit Grover <74338878+harshitgrover-007@users.noreply.github.com>
2022-10-14 14:52:21 +0000

    initial commit message

---
+++ wallet/screens/SignUp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+    View,
+    Text,
+    TouchableOpacity
+} from "react-native";
+
+const SignUp = ()  => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+        </View>
+    )
+}
+
+export default SignUp;
